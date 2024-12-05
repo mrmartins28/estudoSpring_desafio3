0b4c13d8d1cb99e1a917d1563c30da529658c1f6
Leandro Martins <leandro2828@gmail.com>
2024-12-04 22:21:59 -0300

    GET by id e exceptions

--- src/main/java/com/lsmartin/crudcliente/entities/ClientRepository.java
+++ src/main/java/com/lsmartin/crudcliente/entities/ClientRepository.java
@@ -3,5 +3,7 @@ package com.lsmartin.crudcliente.entities;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends  JpaRepository<Client, Long>{
+	
+	
 
 }
--- src/main/java/com/lsmartin/crudcliente/services/ClientService.java
+++ src/main/java/com/lsmartin/crudcliente/services/ClientService.java
@@ -1,5 +1,27 @@
 package com.lsmartin.crudcliente.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lsmartin.crudcliente.dto.ClientDTO;
+import com.lsmartin.crudcliente.entities.Client;
+import com.lsmartin.crudcliente.entities.ClientRepository;
+import com.lsmartin.crudcliente.services.exceptions.ResourceNotFoundException;
+
+@Service
 public class ClientService {
+	
+	@Autowired
+	private ClientRepository clientRepository;
+	
+	@Transactional(readOnly = true)
+	public  ClientDTO findById(Long id) {
+		
+		Client client = clientRepository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recurso não encontrado") );
+		
+		return new ClientDTO(client);
+	}
 
 }
